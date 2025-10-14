0e71ddadd8b8bd6420fbc3ac21e1d8b9894158be
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-10-14 09:19:32 +0000

    Tum start writing wala par hatao ..... samjhe...wo filed esa embed hona

--- src/components/note-editor.tsx
+++ src/components/note-editor.tsx
@@ -120,6 +120,7 @@ export function NoteEditor({ note }: NoteEditorProps) {
         </header>
         
         <EditorToolbar />
+        
         <div className="flex-1 overflow-auto p-4 sm:p-6">
             <Textarea
             value={content}
