e93150325a4fa1075541f248c38c7a720ebb03e1
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-11-21 15:16:29 +0000

    Acha kya tum mujhe mere firebase ka jo jo mujhe environment variables me

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyBpQlj2-nVShq0iiFPaMCXJ79w9ZNz28xc",
-  authDomain: "notesgate-8ba63.firebaseapp.com",
-  projectId: "notesgate-8ba63",
-  storageBucket: "notesgate-8ba63.appspot.com",
-  messagingSenderId: "1010060428959",
-  appId: "1:1010060428959:web:74a9801ac204c22002c4aa",
-  measurementId: "G-CSZQ189HCQ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
